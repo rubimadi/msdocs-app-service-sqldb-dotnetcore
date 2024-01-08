f7692dc4daac4c671ddae7dac6ce0dadf68123f9
rubimadi <alamir.mustafa@gmail.com>
2024-01-08 17:27:18 +0100

    Configure DB & Redis & add migration bundle

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -4,9 +4,15 @@ using DotNetCoreSqlDb.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
-builder.Services.AddDbContext<MyDatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
-builder.Services.AddDistributedMemoryCache();
+builder.Services.AddDbContext<MyDatabaseContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+
+builder.Services.AddStackExchangeRedisCache(options =>
+                                            {
+                                                options.Configuration =
+                                                    builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+                                                options.InstanceName = "SampleInstance";
+                                            });
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -20,8 +26,8 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    // app.UseHsts();
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see
+    // https://aka.ms/aspnetcore-hsts. app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -31,8 +37,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Todos}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Todos}/{action=Index}/{id?}");
 
 app.Run();
